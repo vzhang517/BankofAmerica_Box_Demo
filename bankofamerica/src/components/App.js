38826630ea65a34b28f2723f1528e8a1fbad53c4
Vincent <vzhang517@gmail.com>
2021-04-11 20:12:20 -0500

    initial setup with packages

---
+++ bankofamerica/src/components/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import InputField from "./InputField";
+
+
+
+class App extends React.Component {
+  
+  render() {
+    return (
+      <div className="ui container">
+
+       <InputField/>
+
+
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ bankofamerica/src/components/InputField.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class InputField extends React.Component {
+    render() {
+        return (
+            <div>
+                <form>
+                    <input type="text"/>
+                </form>
+            </div>
+
+        )
+    }
+
+}
+
+export default InputField;
